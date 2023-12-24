c0fcf9a7bd75cc4a7926fdfb7559075bba5110e5
pablo <pabloccsanchez@gmail.com>
2023-12-24 13:32:47 -0500

    chore: implements lambda expressions

--- src/main/java/com/aprendizaje/fundamentos/arraysYColecciones/ConceptoHashSet.java
+++ src/main/java/com/aprendizaje/fundamentos/arraysYColecciones/ConceptoHashSet.java
@@ -1,8 +1,9 @@
 package main.java.com.aprendizaje.fundamentos.arraysYColecciones;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.HashSet;
 import java.util.TreeSet;
+import java.util.LinkedHashSet;
 
 public class ConceptoHashSet {
     public static void main(String[] args) {
@@ -16,28 +17,57 @@ public class ConceptoHashSet {
         System.out.println(conjuntoHash);
 
         System.out.println("""
+                \n
                 HashSet:
-                ---------
-                """);
+                ---------""");
 
-        //recorrer conjunto hash
-        for (String valor : conjuntoHash) {
-            System.out.println(valor);
-        }
+        //recorrer conjunto hash con for-each en una expresión lambda
+        conjuntoHash.forEach(x -> System.out.println(x));
 
-        Set<String> conjutnoTree = new TreeSet<>();
-        conjutnoTree.add("sara");
-        conjutnoTree.add("camila");
-        conjutnoTree.add("pedro");
+        //conjunto TreeHashSet
+        Set<String> conjuntoTree = new TreeSet<>();
+        conjuntoTree.add("sara");
+        conjuntoTree.add("camila");
+        conjuntoTree.add("pedro");
     
         System.out.println("""
+                \n
                 TreeSet:
-                ---------
-                """);
+                ---------""");
         
         //TreeSet imprimirá los valores en orden alfabético
-        for (String valor : conjutnoTree) {
-            System.out.println(valor);
-        }
+        conjuntoTree.forEach(x -> System.out.println(x));
+
+        //conjunto LinkedHashSet
+        Set<Character> conjuntoLinked = new LinkedHashSet<>();
+        conjuntoLinked.add('v');
+        conjuntoLinked.add('r');
+        conjuntoLinked.add('b');
+    
+        System.out.println("""
+                \n
+                LinkedHashSet:
+                ----------------""");
+
+        //LinkedHasSet imprimirá los valores según el orden de inserción
+        //no hay valores duplicados
+        conjuntoLinked.forEach(x -> System.out.println(x));
+
+        //OPERACIONES CON HASH SET
+        //eliminar elementos
+        conjuntoHash.remove("pablo");
+        System.out.println("pablo fué removido del conjunto");
+
+        //verificar existencia de un elemento
+        if (conjuntoHash.contains("pablo")) {
+            System.out.println("pablo está en el conjunto");
+        } else System.out.println("pablo no está en el conjunto");
+
+        //eliminar todos los elementos
+        conjuntoHash.clear();
+        System.out.println("Todos los elementos fueron eliminados");
+
+        //comprobar si el conjunto está vacío
+        if (conjuntoHash.isEmpty()) System.out.println("El conjunto está vacío");
     }
 }
