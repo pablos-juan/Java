2783605789b5b9c948f41c0970c5bdc0aa0c88c8
pablo <pabloccsanchez@gmail.com>
2024-01-11 23:14:48 -0500

    feat: colas in estructurasDeDatos folder

---
+++ src/main/java/com/aprendizaje/estructurasDeDatos/colas/Cola.java
@@ -0,0 +1,71 @@
+package main.java.com.aprendizaje.estructurasDeDatos.colas;
+
+public class Cola {
+
+    private class Nodo {
+        public String dato;
+        public Nodo siguiente;
+
+        public Nodo(String dato) {
+            this.dato = dato;
+            this.siguiente = null;
+        }
+    }
+
+    Nodo cabeza, ultimo;
+    int longitud = 0;
+
+    /**
+     * Introduce un elemento dentro de un nodo y lo agrega
+     * al final de la cola
+     * @param dato Elemento que será agregado
+     */
+    public void encolar(String dato) {
+        Nodo nuevoNodo = new Nodo(dato);
+        /**
+         * Comprobar si la cola esta vacía
+         * Si lo está, apuntar la cabeza hacia el nodo nuevo
+         * Si no está vacía, agregar el elemento nuevo
+         * después del último nodo
+         */
+        if (cabeza == null) {
+            cabeza = nuevoNodo;
+        } else {
+            ultimo.siguiente = nuevoNodo;
+        }
+        // Asignar el nuevo elemento como el final de la cola
+        ultimo = nuevoNodo;
+        longitud++;
+    }
+
+    /**
+     * Elimina el primer elemento de la cola
+     */
+    public void eliminar() {
+        /**
+         * Comprobar si la cola no está vacía
+         * Si no lo está, usar un nodo temporal que señale el primer
+         * elemento de la lista (cabeza) y eliminar su apuntador
+         * Mueva la cabeza hacia el siguiente elemento de la cola
+         */
+        if (cabeza != null) return;
+        Nodo temporal = cabeza;
+        temporal.siguiente = null;
+        cabeza = cabeza.siguiente;
+
+        if (longitud == 0) {
+            // Si la lista está vacía, el último nodo debe esta vacío también
+            ultimo = null;
+        }
+    }
+
+    /**
+     * @return Primer elemento de la cola
+     */
+    public String obtener() {
+        if (cabeza == null) {
+            return null;
+        }
+        return cabeza.dato;
+    }
+}
